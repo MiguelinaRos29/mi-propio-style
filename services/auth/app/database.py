c67f15f919b4cf1d74e8a6ee4f96ad7097d32246
Migue Ross <segisbel@gmail.com>
2026-08-28 12:42:59 +0200

    Limpiar formato de database.py, vaciar ordenes/auth.py (pendiente) y ajustar .gitignore

--- services/auth/app/database.py
+++ services/auth/app/database.py
@@ -3,12 +3,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
@@ -16,3 +16,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
